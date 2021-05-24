ed16b5218cbc9e62a235b6b529af15742e6468f6
smoggy <smoggy@alumni.sjtu.edu.cn>
2021-05-24 10:38:28 +0800

    main node update

--- src/listener_node.cpp
+++ src/listener_node.cpp
@@ -1,25 +1,6 @@
-#include <ros/ros.h>
-#include "sensor_msgs/LaserScan.h"
-#include <algorithm>
-#include"geometry_msgs/Twist.h"
 
-class p_controller{
-public:
-    sensor_msgs::LaserScan input;
-    geometry_msgs::Twist output;
-    void calculate(const double &kp);
-    void callback(const sensor_msgs::LaserScan::ConstPtr &msg);
-};
+#include"p_controller.h"
 
-void p_controller::calculate(const double& kp){
-    double err = 0.0;
-    err = min_element(this->input.ranges.begin(), this->input.ranges.end()) - ((this->input.ranges.begin()) + (this->input.ranges.end() - this->input.ranges.begin())/2);;
-    ROS_INFO("ERROR: [%f]", err);
-    this->output.angular.z = kp * err;
-}
-void p_controller::callback(const sensor_msgs::LaserScan::ConstPtr& msg){
-    this->input = *msg;
-}
 
 int main(int argc, char** argv)
 {
